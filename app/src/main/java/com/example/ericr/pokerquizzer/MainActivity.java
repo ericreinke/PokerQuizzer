95b0bb78ddb5526489b3b8418e12c57d92c1bb46
ericr <ericcreinke@gmail.com>
2018-06-06 17:21:35 -0400

    Added Basic layout to Main and Play Activities

--- app/src/main/java/com/example/ericr/pokerquizzer/MainActivity.java
+++ app/src/main/java/com/example/ericr/pokerquizzer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ericr.pokerquizzer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playGameBtn = (Button) findViewById(R.id.playGameBtn);
+
+        playGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent playIntent = new Intent(MainActivity.this, PlayActivity.class);
+                startActivity(playIntent);
+            }
+        });
     }
 }
