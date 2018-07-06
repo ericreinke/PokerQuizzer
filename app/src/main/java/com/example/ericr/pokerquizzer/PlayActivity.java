f7b3fb9e046196f319dc39f5daa8320bfd92b0c5
ericr <ericcreinke@gmail.com>
2018-07-06 15:35:53 -0400

    Added custom Font

--- app/src/main/java/com/example/ericr/pokerquizzer/PlayActivity.java
+++ app/src/main/java/com/example/ericr/pokerquizzer/PlayActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.os.CountDownTimer;
